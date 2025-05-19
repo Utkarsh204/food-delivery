daecf18cdec0953662b208cc9c7efa93d0396888
Utkarsh yadav <utkarshydv2004@gmail.com>
2025-05-19 09:54:35 +0530

    Update db.js

--- food-del/backend/config/db.js
+++ food-del/backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-     await mongoose.connect('mongodb+srv://yutkarsh39:9012255766@cluster0.pektdgp.mongodb.net/food-del').then(()=>console.log("MongoDB connected"));
+     await mongoose.connect('').then(()=>console.log("MongoDB connected"));
 }
 
 
